dd9edb2feccf7262120d450ef490b6e481a40aa5
Geraldine Jones <49838685+geraldinejns@users.noreply.github.com>
2024-02-22 10:41:36 -0500

    files uploaded to repository

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Geraldine Jones on 2/22/24.
+//
+
+import Foundation
+
+
+struct CaptionChoice {
+    let emoji: String
+    let caption: String
+}
